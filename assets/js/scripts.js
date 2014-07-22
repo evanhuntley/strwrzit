c19c55e6556671774c90912103679ab8c754ba41
Evan (Personal) <evan@evanhuntleysmbp.gateway.2wire.net>
2014-07-22 11:33:02 -0400

    Adding link to full article

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,8 @@
 
-	$(document).ready(function() {	
-		
+	function getWiki() {
+
 		$.ajax({
-		    url: 'http://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json',
+		    url: 'http://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts|info&inprop=url&exchars=500&format=json',
 		    data: {
 		        format: 'json'
 		    },
@@ -12,9 +12,20 @@
 				for (var pageId in data.query.pages) {
 					var excerpt = data.query.pages[pageId].extract;
 					var newexcerpt = excerpt.split('</p>');
-					console.log(newexcerpt[0] + '</p>');
+					var articleLink = '<a href="' + data.query.pages[pageId].fullurl + '">View Full Article</a>';
+					$('article').html(newexcerpt[0] + '</p>' + articleLink);
 				}			
 			}
-		});		
+		});			
+		
+	}
+
+	$(document).ready(function() {	
+			
+		getWiki();
+		
+		$('#button').click(function() {
+			getWiki();
+		});
 		
 	});
