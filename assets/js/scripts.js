347147c5434715a165eea3da92f2653f526d35dc
Evan (Personal) <evan@evanhuntleysmbp.gateway.2wire.net>
2014-07-22 15:25:47 -0400

    Adding normalize, comments on JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,40 @@
+/* ==========================================================================
+	Scripts
+============================================================================= */	
 
+	// Declare general variable
+	var article = $('article');
+
+	// Make the AJAX call, display new content
 	function getWiki() {
-
+		
 		$.ajax({
-		    url: 'http://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts|info&inprop=url&exchars=1000&format=json',
+		    url: 'http://en.wikipedia.org/w/api.php?action=query&generator=random&clshow=!hidden&grnnamespace=0&prop=extracts|info&inprop=url&exchars=1500&format=json',
 		    data: {
 		        format: 'json'
 		    },
 		    dataType: 'jsonp',
 			success: function(data){
-				console.log(data);
 				for (var pageId in data.query.pages) {
 					var excerpt = data.query.pages[pageId].extract;
 					var newexcerpt = excerpt.split('</p>');
 					var articleLink = '<a href="' + data.query.pages[pageId].fullurl + '">View Full Article: "' + data.query.pages[pageId].title + '"</a>';
-					$('article').html(newexcerpt[0] + '</p>' + articleLink).addClass('on');
+					article.html(newexcerpt[0] + '</p>' + articleLink).addClass('on');
 				}			
 			}
 		});			
 		
 	}
 
+	// Document Ready
 	$(document).ready(function() {	
-			
+		
+		// Call Function on refresh	
 		getWiki();
 		
+		// Call Function on click
 		$('#button').click(function() {
-			$('article').removeClass('on');
+			article.removeClass('on');
 			getWiki();
 		});
 		
