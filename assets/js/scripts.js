cc5cb0d3efb2665348b25943a566ab4c7dbcf8b4
Evan (Personal) <evan@evanhuntleysmbp.gateway.2wire.net>
2014-07-22 15:10:48 -0400

    Adding some animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,7 +13,7 @@
 					var excerpt = data.query.pages[pageId].extract;
 					var newexcerpt = excerpt.split('</p>');
 					var articleLink = '<a href="' + data.query.pages[pageId].fullurl + '">View Full Article: "' + data.query.pages[pageId].title + '"</a>';
-					$('article').html(newexcerpt[0] + '</p>' + articleLink);
+					$('article').html(newexcerpt[0] + '</p>' + articleLink).addClass('on');
 				}			
 			}
 		});			
@@ -25,6 +25,7 @@
 		getWiki();
 		
 		$('#button').click(function() {
+			$('article').removeClass('on');
 			getWiki();
 		});
 		
