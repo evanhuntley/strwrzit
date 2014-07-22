de5ed12df852b9b3b5d837e3a075e6c0aee61206
Evan (Personal) <evan@evanhuntleysmbp.gateway.2wire.net>
2014-07-22 10:59:34 -0400

    Outputting first paragraph of random article to the console

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,20 @@
 
-	$(document).ready(function() {
-		
+	$(document).ready(function() {	
 		
 		$.ajax({
-		    url: 'http://en.wikipedia.org/w/api.php?action=query&format=json&prop=langlinks&lllimit=500&titles=kaas',
+		    url: 'http://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json',
 		    data: {
 		        format: 'json'
 		    },
 		    dataType: 'jsonp',
-			success: function( jsonObject ){
-				console.log( jsonObject );
+			success: function(data){
+				console.log(data);
+				for (var pageId in data.query.pages) {
+					var excerpt = data.query.pages[pageId].extract;
+					var newexcerpt = excerpt.split('</p>');
+					console.log(newexcerpt[0] + '</p>');
+				}			
 			}
 		});		
 		
-	
 	});
