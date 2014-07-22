51229ca9471e0067ff74ca70ddb8b6cfb3c45d3c
Evan (Personal) <evan@evanhuntleysmbp.gateway.2wire.net>
2014-07-22 11:34:33 -0400

    Add title display in full article link

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,7 +12,7 @@
 				for (var pageId in data.query.pages) {
 					var excerpt = data.query.pages[pageId].extract;
 					var newexcerpt = excerpt.split('</p>');
-					var articleLink = '<a href="' + data.query.pages[pageId].fullurl + '">View Full Article</a>';
+					var articleLink = '<a href="' + data.query.pages[pageId].fullurl + '">View Full Article: "' + data.query.pages[pageId].title + '"</a>';
 					$('article').html(newexcerpt[0] + '</p>' + articleLink);
 				}			
 			}
